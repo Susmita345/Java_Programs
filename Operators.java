9d072d845e492f3b86263f1a5211e126bec5e040
Susmita345 <115895024+Susmita345@users.noreply.github.com>
2023-11-10 23:26:25 +0530

    Add files via upload

---
+++ Operators.java
@@ -0,0 +1,22 @@
+class Operators{
+    public static void main(String[] args){
+        //Question 1.The value of x^x gives "0":
+        int x=1;
+        System.out.println(x^x);
+        //Question 2.Swapping:
+        int a=1;
+        int b=2;
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println("swapped values:"+a+" "+b);
+        //Question 3. Addimg 1 to an integer:
+        int c=12;
+        System.out.println(~c);
+        //Question 4. Conversion of Uppercase to Lowercase:
+        char s='R';
+        System.out.println(Character.toLowerCase(u));
+        
+
+    }
+}
