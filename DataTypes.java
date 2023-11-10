56c51b1e8b51cf3d20af78137403df847c9f232e
Susmita345 <115895024+Susmita345@users.noreply.github.com>
2023-11-10 22:47:09 +0530

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,23 @@
+class DataTypes{
+    public static void main(String[]args){
+        //Question 1.
+        String[] a={"Susmita","Anjali","Rushank"};
+        System.out.println(a[0]);
+        //Question 2 and 3
+        char[] b={10,20,30,40};
+        System.out.println(b[3]);
+        //Question 4
+        String c="Ufeus";
+        //char d=c.charAt(0);
+        System.out.println(c.charAt(4));
+        //Question 5 and 6 Type cast:
+        byte e=12;
+        int f=e;
+        System.out.println(f);
+        long g=99898;
+        short h=g;
+        System.out.println(h);
+
+
+    }
+}
